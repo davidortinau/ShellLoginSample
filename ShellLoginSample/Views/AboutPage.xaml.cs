64a1613e3e167c46f3c6efe9dd48a758e067b743
David Ortinau <david.ortinau@microsoft.com>
2019-07-23 12:47:23 -0500

    Shell login sample from live stream

---
+++ ShellLoginSample/Views/AboutPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShellLoginSample.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void OnLogout(object source, EventArgs args)
+        {
+            await Shell.Current.GoToAsync("//login");
+        }
+    }
+}
---
+++ ShellLoginSample/Views/LoginPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ShellLoginSample.Views
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async void OnClicked(object source, EventArgs args)
+        {
+            await Shell.Current.GoToAsync("//main");
+        }
+    }
+}
